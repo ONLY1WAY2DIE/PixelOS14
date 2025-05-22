b00b9a583846bc5acc535eed9a5a708454cf8a71
ONLY1WAY2DIE <124692093+ONLY1WAY2DIE@users.noreply.github.com>
2025-05-22 12:19:46 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+export DEVICE=panther
+export ROM_MANIFEST="https://github.com/PixelOS-AOSP/manifest"
+export ROM_BRANCH="uvite"
+export BUILD_TYPE=userdebug
+
+sudo apt-get update && sudo apt-get install -y git-core repo curl zip unzip python3 python3-pip
+git config --global user.name "ONLY1WAY2DIE"
+git config --global user.email "dev@only1way2die.example"
+
+mkdir -p ~/android/rom && cd ~/android/rom
+repo init -u $ROM_MANIFEST -b $ROM_BRANCH --git-lfs --depth=1
+mkdir -p .repo/local_manifests
+cp /path/to/local_manifests/pixelos_panther_a14.xml .repo/local_manifests/
+repo sync -j4
+source build/envsetup.sh
+lunch aosp_panther-$BUILD_TYPE
+mka bacon
